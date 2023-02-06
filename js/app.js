14f9cdedc405d3625260e1b33fa2834a6e91a308
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-02-06 16:48:07 +0100

    FEAT slider conditions added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 console.log('hello')
 
+const slides = ['./img/01.jpg', './img/02.jpg', './img/03.jpg', './img/04.jpg', './img/05.jpg']
+
+
 // GET elements from HTML
 
 let slideElements = document.querySelectorAll('.slide-thumb')
@@ -20,54 +23,58 @@ let slideCounter = 0
 rightSlideElement.addEventListener('click', function () {
     console.log('current slide', slideCounter)
 
-    // click = 0 
-    let actualSlide = slideElements[slideCounter]
-
-    // removing class active from the active element
-    actualSlide.classList.remove('active-slide')
-
-    // click + 1
-    slideCounter += 1 
-
-    // click = 1 of 4
-    let nextSlide = slideElements[slideCounter]
-
-    // adding class active to the next element
-    nextSlide.classList.add('active-slide')
-
-
     // WHEN i get to the last element of the slideElement it doesn't increase anymore
 
-    console.log('next slide', slideCounter)
+    if (slideCounter < 4) {
+
+        // click = 0 
+        let actualSlide = slideElements[slideCounter]
+
+        // removing class active from the active element
+        actualSlide.classList.remove('active-slide')
+
+        // click + 1
+        slideCounter += 1 
+
+        // click = 1 of 4
+        let nextSlide = slideElements[slideCounter]
+
+        // adding class active to the next element
+        nextSlide.classList.add('active-slide')
+
+        console.log('next slide', slideCounter)
+
+    }
 
 })
-
-
-
 
 // WHEN I click on left arrow i get to the previous element
 
 leftSlideElement.addEventListener('click', function () {
     console.log('current slide', slideCounter)
 
-    // click = 0
-    let actualSlide = slideElements[slideCounter]
-
-    // removing class active from the active element
-    actualSlide.classList.remove('active-slide')
-
-    // click - 1
-    slideCounter -= 1 
-
-    // click = -1 of 4
-    let previousSlide = slideElements[slideCounter]
-
-    // adding class active to the next element
-    previousSlide.classList.add('active-slide')
-
     // WHEN i get to the first element of the slideElement is doesn't decrease anymore
 
-    console.log('previous slide', slideCounter)
+    if (slideCounter > 0) {
+
+        // click = 0
+        let actualSlide = slideElements[slideCounter]
+
+        // removing class active from the active element
+        actualSlide.classList.remove('active-slide')
+
+        // click - 1
+        slideCounter -= 1 
+
+        // click = -1 of 4
+        let previousSlide = slideElements[slideCounter]
+
+        // adding class active to the next element
+        previousSlide.classList.add('active-slide')
+
+        console.log('previous slide', slideCounter)
+
+    }
 
 })
 
